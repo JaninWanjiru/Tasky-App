78b48aaa8066a303d890b5ffd898d8e4b5403920
JaninWanjiru <janinndongo02@gmail.com>
2025-07-18 15:45:24 +0300

    add endpoint for logging in a user

--- Server/src/controllers/auth.controller.ts
+++ Server/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
 
 const client = new PrismaClient();
 
@@ -13,6 +14,38 @@ export const register = async (req: Request, res: Response) => {
       data: { firstName, lastName, username, email, password: hashedPass },
     });
     res.status(201).json({ message: "User created successfully" });
+  } catch (e) {
+    res.status(500).json({ message: "There was a hiccup on our end. Please try again." });
+  }
+};
+
+// Logging in a user
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { identifier, password} = req.body; 
+    const user = await client.user.findFirst({
+      where: {
+        OR: [
+          { email: identifier },
+          { username: identifier }
+        ]
+      }
+    });
+
+    if (!user) {
+      res.status(401).json({ message: "Login credentials not correct" });
+      return;
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      res.status(401).json({ message: "Login credentials not correct" });
+      return;
+    }
+
+    const {password: userPassword, dateJoined, lastprofileUpdate, ...userDetails} = user
+    const token = jwt.sign(userDetails, process.env.JWT_SECRET!)
+    res.cookie("authToken", token).json(userDetails)
   } catch (e) {
     res.status(500).json({ message: "There was a hiccup on our end. Please try again." });
   }
--- Server/src/routes/auth.route.ts
+++ Server/src/routes/auth.route.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import verifyUserRegistration from "../middlewares/validateUserRegistration";
 import verifyPassStrength from "../middlewares/validatePasswordStrength";
 import checkDuplicateUser from "../middlewares/checkDuplicateUser";
-import { register } from "../controllers/auth.controller";
+import { register, login } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", verifyUserRegistration, checkDuplicateUser, verifyPassStrength,  register);
 
+router.post('/login', login)
 export default router;
