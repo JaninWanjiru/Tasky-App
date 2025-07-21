03c277aa9762e82150b852243eba219737b803b3
JaninWanjiru <janinndongo02@gmail.com>
2025-07-21 12:35:55 +0300

    add middleware validateTask, that ensures that no empty tasks are added

---
+++ Server/src/middlewares/validateTask.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+
+function validateTask(req:Request, res:Response, next: NextFunction) {
+    const {title, description} = req.body;
+
+    if (!title) {
+        res.status(400).json({message: 'Title is required'})
+        return;
+    }
+    if (!description) {
+        res.status(400).json({message: 'Description is required'})
+        return;
+    }
+    next();
+}
+
+export default validateTask;
--- Server/src/routes/tasks.route.ts
+++ Server/src/routes/tasks.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { createTask } from "../controllers/tasks.controller";
 import userAuth from "../middlewares/userAuth";
-
+import validateTask from "../middlewares/validateTask";
 const router = Router()
 
-router.post('/', userAuth, createTask);
+router.post('/', userAuth, validateTask, createTask);
 
 export default router; 
