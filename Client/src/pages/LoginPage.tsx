8c51be55d27c8859989d79309d775d4b43182ed0
JaninWanjiru <janinndongo02@gmail.com>
2025-07-23 11:50:12 +0300

    add intergration logic for the Login Page - setup a zustand store for tracking the user info globally

--- Client/src/pages/LoginPage.tsx
+++ Client/src/pages/LoginPage.tsx
@@ -1,7 +1,46 @@
-import { Link } from "react-router-dom";
-import { Box, Card, CardContent, Typography, TextField, Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { Box, Card, CardContent, Typography, TextField, Button, Alert, InputAdornment, IconButton } from "@mui/material";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import axiosInstance from "../api/axios";
+import axios from "axios";
+import useUser from "../store/userStore";
+import { FaRegFaceRollingEyes } from "react-icons/fa6";
+import { PiSmileyXEyes } from "react-icons/pi";
 
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const {setUser} =useUser()
+  const [identifier, setIdentifier] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {isPending, mutate} = useMutation({
+    mutationKey: ["Login-user"],
+    mutationFn: async (LoginData: { identifier: string; password: string}) => {
+      const res = await axiosInstance.post("/api/auth/login", LoginData);
+      return res.data;
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data.message);
+      } else {
+        setError("There was a hiccup on our end. Please try again.");
+      } 
+    },
+    onSuccess: (data) => {
+      setUser(data)
+      navigate("/tasks");
+    },
+  })
+
+  function handleLogin() {
+    setError("")
+    mutate({identifier, password})
+  }
+  
   return (
     <Box component="form" sx={{ bgcolor: "#fff", px: { xs: 2, sm: 0 } }}>
       <Card elevation={3} sx={{ maxWidth: 400, mx: "auto", mt: 8, borderRadius: 4 }}>
@@ -12,6 +51,9 @@ function LoginPage() {
           <Typography color="text.secondary" align="center" sx={{ mb: 3 }}>
             Sign in to Tasky and get things in order
           </Typography>
+
+          {error && <Alert severity="error" sx={{ mb: 3 }}>{error}</Alert>}
+          
           <Box>
             <TextField
               size="small"
@@ -19,23 +61,38 @@ function LoginPage() {
               placeholder="Enter your email or username"
               fullWidth
               required
+              value={identifier}
+              onChange={(e) => setIdentifier(e.target.value)}
               sx={{ mb: 2 }}
             />
             <TextField
               size="small"
               label="Password"
               name="password"
-              type="password"
+              type={showPassword ? "text" : "password"}
               placeholder="Enter your password"
               fullWidth
               required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               sx={{ mb: 2 }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={() => setShowPassword((prev) => !prev)} edge="end">
+                      {showPassword ? <FaRegFaceRollingEyes /> : <PiSmileyXEyes />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
             />
             <Button
               type="button"
               variant="contained"
               color="primary"
               fullWidth
+              onClick={handleLogin}
+              loading={isPending}
               size="small"
               sx={{ fontWeight: "bold", fontSize: 15, borderRadius: 2, mb: 2 }}
             >
