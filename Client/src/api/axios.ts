e44e818d2674705677b21775a2fd900f69c9b6bb
JaninWanjiru <janinndongo02@gmail.com>
2025-07-28 11:15:16 +0300

    add /api in the baseURL

--- Client/src/api/axios.ts
+++ Client/src/api/axios.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://tasky-app-zy17.onrender.com",
+  baseURL: "https://tasky-app-zy17.onrender.com/api",
   withCredentials: true,
 });
 // const axiosInstance = axios.create({
-//   baseURL: "http://localhost:4650", 
+//   baseURL: "http://localhost:4650",
 //   withCredentials: true,
 // });
 
